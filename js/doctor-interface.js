e870bf7725d766baf7acf25411cc91ab8ad65102
Daniel <dlopez6877@gmail.com>
2017-06-30 10:49:47 -0700

    create form with inputs for user name, city, and state. add basic styling to form with overlay. use google maps api to convert city and state to latitude and longitude

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,7 +2,21 @@
 var Search = require('./../js/doctor.js').searchModule;
 
 $(document).ready(function() {
-  // var newSearch = new Search();
-  // newSearch.getDoctors();
+
+  $('#search-location').submit(function(event) {
+    event.preventDefault();
+
+    var city = $('#city').val();
+    var state = $('#state').val();
+
+    $('#first-name').val("");
+    $('#last-name').val("");
+    $('#city').val("");
+    $('#state').val("");
+    $('#zip').val("");
+
+    var newSearch = new Search();
+    newSearch.getLocation(city, state);
+  });
 
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,8 +1,30 @@
 var apiKey = require('./../.env').apiKey;
+var apiKeyGoogle = require('./../.env').apiKeyGoogle;
 
 Search = function() {
 
 };
+
+Search.prototype.getLocation = function(city, state) {
+  // $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + state + '&key=' + apiKey).then(function(response) {
+  //   var lat = object.results[0].geometry.location.lat;
+  //   var lon = object.results[0].geometry.location.lng;
+  //   console.log(city, state, lat, lon);
+  //   Search.getDoctors(lat, lon);
+  //   console.log(response);
+  // }).fail(function(error) {
+  //   console.log(error);
+  // });
+
+  $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + state + '&key=' + apiKeyGoogle).then(function(response) {
+    console.log(response);
+    var lat = response.results[0].geometry.location.lat;
+    var lon = response.results[0].geometry.location.lng;
+    console.log(city, state, lat, lon);
+  }).fail(function(error) {
+    console.log(error);
+  });
+}
 
 Search.prototype.getDoctors = function() {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=' + apiKey)
