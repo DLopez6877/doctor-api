6318d33b63f6aaa8d2676f5fb4d4c20e013a447e
Daniel <dlopez6877@gmail.com>
2017-06-30 09:46:14 -0700

    add full-screen background image and remove document ready api call test

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,7 +2,7 @@
 var Search = require('./../js/doctor.js').searchModule;
 
 $(document).ready(function() {
-  var newSearch = new Search();
-  newSearch.getDoctors();
+  // var newSearch = new Search();
+  // newSearch.getDoctors();
 
 });
--- js/doctor.js
+++ js/doctor.js
@@ -2,7 +2,7 @@ var apiKey = require('./../.env').apiKey;
 
 Search = function() {
 
-}
+};
 
 Search.prototype.getDoctors = function() {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=' + apiKey)
