f530fb77a8c9b511651217ada9e8af9fe1983ada
Daniel <dlopez6877@gmail.com>
2017-06-30 09:32:04 -0700

    * create .env folder and export api-key run test api call to console log return during document load

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,3 +1,8 @@
+
+var Search = require('./../js/doctor.js').searchModule;
+
 $(document).ready(function() {
+  var newSearch = new Search();
+  newSearch.getDoctors();
 
 });
---
+++ js/doctor.js
@@ -0,0 +1,17 @@
+var apiKey = require('./../.env').apiKey;
+
+Search = function() {
+
+}
+
+Search.prototype.getDoctors = function() {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+};
+
+exports.searchModule = Search;
