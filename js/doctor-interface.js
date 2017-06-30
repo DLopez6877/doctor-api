fce5640e57bf2cf85595ba038bc8cf62f0c3b206
Daniel <dlopez6877@gmail.com>
2017-06-30 13:21:43 -0700

    setup structure for appending result

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,5 +1,30 @@
-
 var Search = require('./../js/doctor.js').searchModule;
+
+var displayResults = function(result) {
+  console.log('test');
+  $('#doctors').empty();
+  $('#appended-features').empty();
+  result.data.forEach(function (i) {
+    $('#doctors').append('<li> Dr. ' + i.profile.first_name + " " + i.profile.last_name + " " + i.profile.title);
+    $('#appended-features').append('<li>hey</li>'
+
+    );
+  });
+}
+
+// <div class="feature">
+//   <h1>[doctor name]</h1>
+//   <p id="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+//   <p>icon* [Street] [Street2], [City], [State], [Zip]</p>
+//   <p>icon* [hours]</p>
+//   <p>icon* [phone]</p>
+//   <p>icon* [gender]</p>
+//   <p class="lead">Specialties:</p>
+//   <ul>
+//     <li>[specialty]</li>
+//     <li>[specialty]</li>
+//   </ul>
+// </div>
 
 $(document).ready(function() {
 
@@ -16,7 +41,7 @@ $(document).ready(function() {
     $('#zip').val("");
 
     var newSearch = new Search();
-    newSearch.getLocation(city, state);
+    newSearch.getLocation(city, state, displayResults);
   });
 
 });
--- js/doctor.js
+++ js/doctor.js
@@ -4,28 +4,29 @@ Search = function() {
 
 };
 
-Search.prototype.getLocation = function(city, state) {
+Search.prototype.getLocation = function(city, state, displayResults) {
   var search = this;
   $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + state).then(function(response) {
     var lat = response.results[0].geometry.location.lat;
     var lon = response.results[0].geometry.location.lng;
-    search.round(lat, lon);
+    search.round(lat, lon, displayResults);
   }).fail(function(error) {
     console.log("google fail");
   });
 };
 
-Search.prototype.round = function(lat, lon) {
+Search.prototype.round = function(lat, lon, displayResults) {
   var search = this;
   var roundedLat = Math.ceil(lat * 1000) / 1000;
   var roundedLon = Math.ceil(lon * 1000) / 1000;
-  search.getDoctors(roundedLat, roundedLon);
+  search.getDoctors(roundedLat, roundedLon, displayResults);
 };
 
-Search.prototype.getDoctors = function(roundedLat, roundedLon) {
+Search.prototype.getDoctors = function(roundedLat, roundedLon, displayResults) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=' + roundedLat + '%2C' + roundedLon + '%2C100&sort=distance-asc&skip=0&limit=20&user_key=' + apiKey)
   .then(function(result) {
     console.log(result);
+    displayResults(result);
   })
   .fail(function(error){
     console.log("doctor fail");
