60946e051fab269f4f215b10e26e2a2c66dad284
Daniel <dlopez6877@gmail.com>
2017-06-30 19:47:05 -0700

    remove required from zip. add x in top right corner of containers to close overlay and return to splash screen

--- js/cta-interface.js
+++ js/cta-interface.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
 
   $('#cta').click(function(event) {
     event.preventDefault();
-    $('.main-text').fadeOut();
+    $('.main-text').hide();
     $('.overlay').css('display', 'flex');
     $('#location-container').css('display', 'flex');
   });
 
+  $('.cta-close').click(function() {
+    $('.main-text').show();
+    $('.overlay').hide();
+  });
 });
