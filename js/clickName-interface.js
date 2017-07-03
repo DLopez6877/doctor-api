c45918ebe156d5f10ef437ae722db388e2931fec
Daniel <dlopez6877@gmail.com>
2017-07-03 00:56:53 -0700

    fix bug that caused doctors with same name to be displayed at the same time

--- js/clickName-interface.js
+++ js/clickName-interface.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-
   $(document).on('click', '#doctors li', function() {
     var selectedObject =$(this).text();
     $('h1').each(function(i) {
       if ($(this).text() === selectedObject) {
+        $('.feature').hide();
         $(this).parent().show();
-      } else {
-        $(this).parent().hide();
+        return false;
       }
     });
-  }) ;
+  });
 });
